0d6174f6c1a767c8a5923e5db603b8854c206014
Niklas Livfors <niklas.livfors@gmail.com>
2017-08-21 14:52:58 +0200

    Add project files.

---
+++ Networking/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                }
+            }
+
+            Console.WriteLine($"IP: {localIP}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Networking2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Networking2
+{
+    class Program
+    {
+        static void Main()
+        {
+            new Thread(Server).Start();       
+            Thread.Sleep(500);
+            Client();
+
+            Console.ReadKey();
+        }
+
+        static void Client()
+        {
+            using (TcpClient client = new TcpClient("localhost", 51111))
+
+            using (NetworkStream n = client.GetStream())
+            {
+                BinaryWriter w = new BinaryWriter(n);
+                w.Write("Hello");
+                w.Flush();
+                Console.WriteLine(new BinaryReader(n).ReadString());
+            }
+        }
+
+        static void Server()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 51111);
+            listener.Start();
+            using (TcpClient c = listener.AcceptTcpClient())
+            using (NetworkStream n = c.GetStream())
+            {
+                string msg = new BinaryReader(n).ReadString();
+                BinaryWriter w = new BinaryWriter(n);
+                w.Write(msg + " right back!");
+                w.Flush();
+            }
+            listener.Stop();
+        }
+    }
+}
