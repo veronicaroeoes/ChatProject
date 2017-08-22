d0781e49953f6a40912379e8be4e6c2fc0f3cc51
Niklas Livfors <niklas.livfors@gmail.com>
2017-08-22 16:13:20 +0200

    Vem vet...

--- Networking_client/Program.cs
+++ Networking_client/Program.cs
@@ -17,10 +17,60 @@ namespace Networking_client
         {
             //Client myClient = new Client();
 
-            //Thread clientThread = new Thread(myClient.Start);
-            //clientThread.Start();
-            //clientThread.Join();
+            Thread clientThread = new Thread(myClient.Start);
+            clientThread.Start();
+            clientThread.Join();
         }
+
+        public class Client
+        {
+            private TcpClient client;
+
+            public void Start()
+            {
+                //client = new TcpClient("192.168.25.126", 5000);
+                //client = new TcpClient("192.168.25.126", 5000);
+                //client = new TcpClient("192.168.25.116", 5000); //Veronica
+                client = new TcpClient("192.168.25.126", 5000); //Niklas L
+
+                Thread listenerThread = new Thread(Send);
+                listenerThread.Start();
+
+                Thread senderThread = new Thread(Listen);
+                senderThread.Start();
+
+                senderThread.Join();
+                listenerThread.Join();
+            }
+
+            public void Listen()
+            {
+                string message = "";
+
+                try
+                {           
+                    while (true)
+                    {
+                        NetworkStream n = client.GetStream();
+                        message = new BinaryReader(n).ReadString();
+                        Console.WriteLine("Other: " + message);                       
+                    }                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            public void Send()
+            {
+                string message = "";
+
+                try
+                {
+                    while (!message.Equals("quit"))
+                    {
+                        NetworkStream n = client.GetStream();
 
        
     }
