e432e6aaed32b65c741009c506904fbfdd2e1127
Niklas Livfors <niklas.livfors@gmail.com>
2017-08-22 16:14:06 +0200

    Vem vet

--- Networking_server/Server.cs
+++ Networking_server/Server.cs
@@ -66,7 +66,7 @@ namespace Networking_server
 
             string jsonmessage = JsonConvert.SerializeObject(listOfUsers);
 
-            Broadcast(clientHandler, jsonmessage);
+            Broadcast(clientHandler, jsonmessage, clientHandler.UserName);
 
         }
 
